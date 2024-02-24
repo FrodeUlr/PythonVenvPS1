44dbf171af4700ec4235866e40988957cf997c6a
Frode Ulriksen <frode.ulriksen@outlook.com>
2024-02-24 20:12:49 +0100

    Corrected delete

--- 00_Scripts/DeleteVenv.ps1
+++ 00_Scripts/DeleteVenv.ps1
@@ -3,12 +3,12 @@ $directories = Get-ChildItem -Path $path -Directory
 Write-Host "Chose the environment to delete"
 $directories | ForEach-Object -Begin { $i = 1 } -Process { Write-Host "$i. $($_.Name)"; $i++ }
 [int]$choice = Read-Host "Enter the number of the environment to delete"
-if ($choice -ge 0 -and $choice -lt $directories.Count) {) {
+if ($choice -ge 1 -and $choice -eq $directories.Count) {
     $venv = $directories[$choice - 1]
     Write-Host "Deleting environment $venv"
     Remove-Item -Path $venv.FullName -Recurse -Force
     exit 0
-})
+}
 else {
     Write-Host "Invalid choice"
     exit 1
