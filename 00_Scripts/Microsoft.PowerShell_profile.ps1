73e60ad0ecd7883bcad56111333a0ca416d3dcee
Frode Ulriksen <Frode.Ulriksen@cegal.com>
2025-01-28 10:04:26 +0100

    Update with environment variable paths

--- 00_Scripts/Microsoft.PowerShell_profile.ps1
+++ 00_Scripts/Microsoft.PowerShell_profile.ps1
@@ -1,13 +1,14 @@
-oh-my-posh init pwsh --config 'C:\Users\frodeul\AppData\Local\Programs\oh-my-posh\themes\lightgreen2.omp.json' | Invoke-Expression
+oh-my-posh init pwsh --config "$env:LOCALAPPDATA\Programs\oh-my-posh\themes\lightgreen2.omp.json" | Invoke-Expression
 Import-Module posh-git
 Set-PSReadLineKeyHandler -Key Tab -Function MenuComplete
 Import-Module -Name Terminal-Icons
-
-Set-Alias activate "D:\00_Privat_Git\PythonVenvPS1\00_Scripts\ActivateVenv.ps1"
-Set-Alias create "D:\00_Privat_Git\PythonVenvPS1\00_Scripts\CreateVenv.ps1"
-Set-Alias list "D:\00_Privat_Git\PythonVenvPS1\00_Scripts\ListVenv.ps1"
+$scriptpath = $env:PRIV_PYTHON_SCRIPTS
+$venvpath = $env:PRIV_PYTHON_LOC
+Set-Alias activate "$scriptpath\ActivateVenv.ps1"
+Set-Alias create "$scriptpath\CreateVenv.ps1"
+Set-Alias list "$scriptpath\ListVenv.ps1"
+Set-Alias delete "$scriptpath\DeleteVenv.ps1"
 Set-Alias grep "Select-String"
-Set-Alias delete "D:\00_Privat_Git\PythonVenvPS1\00_Scripts\DeleteVenv.ps1"
 Set-Alias pipclean "uv pip freeze > unins ; uv pip uninstall -r unins ; del unins"
 
-& "D:\00_Privat_Git\PythonVenvPS1\01_Venv\default\Scripts\Activate.ps1"
+& "$venvpath\default\Scripts\Activate.ps1"
